1c983883376cefb6c8cf87956e79136f2a27dc9f
Joe Hannis <45033532+joehannis@users.noreply.github.com>
2025-09-26 17:15:03 +0100

    Delete ZendeskAI directory

--- ZendeskAI/utils/timeout.mjs
+++
@@ -1,8 +0,0 @@
-import { Agent } from 'undici';
-
-export const dispatcher = new Agent({
-  headersTimeout: 900_000,
-  bodyTimeout: 900_000,
-  connect: { timeout: 900_000 },
-  autoSelectFamily: true,
-});
